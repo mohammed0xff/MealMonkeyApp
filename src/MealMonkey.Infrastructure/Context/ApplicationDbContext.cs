28a86c506971ba32cbe0e0c06dbbac289c824247
mohammed0xff <mohammed0xff@gmail.com>
2023-04-12 03:17:11 +0200

    add : initial project files

---
+++ src/MealMonkey.Infrastructure/Context/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using EntityFramework.Exceptions.SqlServer;
+using Microsoft.EntityFrameworkCore;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        base.OnConfiguring(optionsBuilder);
+        optionsBuilder.UseExceptionProcessor();
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+    }
+    
+}
