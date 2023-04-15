d25979392b35fa177d7f22ce5ec187e47d0016b6
mohammed0xff <mohammed0xff@gmail.com>
2023-04-16 01:39:33 +0200

    add user configurations

---
+++ src/MealMonkey.Infrastructure/Configurations/UserConfigurations.cs
@@ -0,0 +1,45 @@
+﻿using MealMonkey.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace MealMonkey.Infrastructure.Configurations
+{
+    public class UserConfigurations : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            
+            builder.Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode();
+            
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(50)
+                .IsUnicode();
+            
+            builder.Property(u => u.PhoneNumber)
+                .HasMaxLength(20);
+            
+            builder.Property(u => u.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+            
+            builder.Property(u => u.HashPassword)
+                .IsRequired()
+                .HasMaxLength(200);
+            
+            builder.Property(u => u.CreatedAt)
+                .IsRequired()
+                .HasDefaultValue(DateTime.Now);
+
+            // relations 
+/*            builder.HasOne(u => u.Address)
+                .WithMany()
+                .HasForeignKey(u => u.AddressId);*/
+
+        }
+    }
+}
