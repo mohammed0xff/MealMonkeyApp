6907d07283338db26409f8ea947b575cf26d7dc8
farouk4web <faroukabdelhamid.eg@gmail.com>
2023-04-17 17:50:50 +0200

    Add CityConfiguration

---
+++ src/MealMonkey.Infrastructure/Configurations/CityConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using MealMonkey.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealMonkey.Infrastructure.Configurations
+{
+    public class CityConfigurations : IEntityTypeConfiguration<City>
+    {
+        public void Configure(EntityTypeBuilder<City> builder)
+        {
+            // Properties
+            builder.Property(x => x.Name)
+                .HasMaxLength(15)
+                .IsRequired();
+        }
+    }
+}
