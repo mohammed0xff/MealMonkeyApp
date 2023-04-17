54631e58e6f952b11c49b21cc7ec4e4c7350d679
farouk4web <faroukabdelhamid.eg@gmail.com>
2023-04-17 18:00:19 +0200

    Add MealConfiguration

---
+++ src/MealMonkey.Infrastructure/Configurations/MealConfigurations.cs
@@ -0,0 +1,47 @@
+﻿using MealMonkey.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealMonkey.Infrastructure.Configurations
+{
+    public class MealConfigurations : IEntityTypeConfiguration<Meal>
+    {
+        public void Configure(EntityTypeBuilder<Meal> builder)
+        {
+            builder.Property(m => m.Name)
+                .HasMaxLength(40)
+                .IsRequired();
+
+
+            // Relationships
+            // Category
+            builder.HasOne<Category>(m => m.Category)
+                .WithMany(c => c.Meals)
+                .HasForeignKey(m => m.CategoryId)
+                .IsRequired();
+
+            // Resturant
+            builder.HasOne<Resturant>(m => m.Resturant)
+                .WithMany(m => m.Meals)
+                .HasForeignKey(m => m.ResturantId)
+                .IsRequired();
+
+            // MealType
+            builder.HasOne<MealType>(m => m.MealType)
+                .WithMany(m => m.Meals)
+                .HasForeignKey(m => m.MealTypeId)
+                .IsRequired();
+
+            // Review 
+            builder.HasMany<Review>(m => m.Reviews)
+                .WithOne<Meal>(r => r.Meal)
+                .HasForeignKey(r => r.MealId);
+        }
+    }
+}
