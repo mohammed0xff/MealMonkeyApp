0b5d9bf0fcdc85d9522d49163d771b20b64e871b
Ahmed Muhammed Youssef <ahmed.muhammed.youssef@gmail.com>
2023-04-17 20:56:28 +0200

    refactor: Remove unused usings

--- src/MealMonkey.Infrastructure/Configurations/MealConfigurations.cs
+++ src/MealMonkey.Infrastructure/Configurations/MealConfigurations.cs
@@ -1,12 +1,6 @@
 ﻿using MealMonkey.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MealMonkey.Infrastructure.Configurations
 {
