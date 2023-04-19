c95c1cbf2131dce73e508ecea66f06f76fa6dba7
farouk4web <faroukabdelhamid.eg@gmail.com>
2023-04-19 06:54:48 +0200

    Create Status Enum To Store status Names (#7)

---
+++ src/MealMonkey.Domain/Enums/Status.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MealMonkey.Domain.Enums
+{
+    public enum Status
+    {
+        Confirmed, InKitchen, InDelivery, Success, Canceled
+    }
+}
