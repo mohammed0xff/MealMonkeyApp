998d04ee4003f3c35ee176f19b1850406159ed04
mohammed0xff <mohammed0xff@gmail.com>
2023-04-24 17:08:36 +0200

    feat : add `ITokenService` and `TokenService` to generate and refresh tokens (#14)

---
+++ src/MealMonkey.Application/Services/Token/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using MealMonkey.Application.Common;
+using MealMonkey.Domain.Entities.UserEntities;
+
+namespace MealMonkey.Application.Services.Token
+{
+    public interface ITokenService
+    {
+        public Task<TokenResponse?> GenerateToken(User user);
+        public Task<TokenResponse?> TryRefreshToken(RefreshTokenRequest requst);
+    }
+}
