d1650b6f29885ca77d052b1f0e28eeb3478f2c03
mohammed0xff <mohammed0xff@gmail.com>
2023-04-24 17:08:13 +0200

    feat : add `TokenResponse` and `RefreshTokenRequest` (#14)

---
+++ src/MealMonkey.Application/Common/RefreshTokenRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace MealMonkey.Application.Common
+{
+    public class RefreshTokenRequest
+    {
+        public string Email { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime ExpiresIn { get; set; }
+    }
+}
