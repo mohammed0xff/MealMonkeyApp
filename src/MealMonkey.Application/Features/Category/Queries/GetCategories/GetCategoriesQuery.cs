7af202065e5d0a2e942e3db02410f544859850f6
mohammed0xff <mohammed0xff@gmail.com>
2023-05-06 00:19:42 +0300

    feat : add category queries (#18)

---
+++ src/MealMonkey.Application/Features/Category/Queries/GetCategories/GetCategoriesQuery.cs
@@ -0,0 +1,11 @@
+﻿using MealMonkey.Application.Common.Pagination;
+using MealMonkey.Domain.Entities.MealEntities;
+using MediatR;
+
+namespace MealMonkey.Application.Features.CategoryFeatures.Queries.GetCategories
+{
+    public class GetCategoriesQuery : PaginatedRequest, IRequest<PaginatedListResponse<Category>>
+    {
+    }
+
+}
---
+++ src/MealMonkey.Application/Features/Category/Queries/GetCategoryById/GetCategoriesQuery.cs
@@ -0,0 +1,10 @@
+﻿using MealMonkey.Domain.Entities.MealEntities;
+using MediatR;
+
+namespace MealMonkey.Application.Features.CategoryFeatures.Queries.GetCategories
+{
+    public class GetCategoryByIdQuery : IRequest<Category>
+    {
+        public Guid Id { get; set; }
+    }
+}
