48be6a24eddc130f9eca94289dd07b5242360c71
mohammed0xff <mohammed0xff@gmail.com>
2023-05-06 00:19:59 +0300

    feat : add category commands (#18)

---
+++ src/MealMonkey.Application/Features/Category/Commands/CreateCategory/CreateCategoryHandler.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MediatR;
+using MealMonkey.Domain.Entities.MealEntities;
+
+namespace MealMonkey.Application.Features.CategoryFeatures.Commands.CreateCategory
+{
+    public class CreateCategoryCommandHandler : IRequestHandler<CreateCategoryCommand, Category>
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CreateCategoryCommandHandler(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Category> Handle(CreateCategoryCommand request, CancellationToken cancellationToken)
+        {
+            var category = new Category
+            {
+                Id = Guid.NewGuid(),
+                Name = request.Name
+            };
+
+            await _dbContext.Categories.AddAsync(category, cancellationToken);
+            await _dbContext.SaveChangesAsync(cancellationToken);
+
+            return category;
+        }
+    }
+}
---
+++ src/MealMonkey.Application/Features/Category/Commands/DeleteCategory/DeleteCategoryCommand.cs
@@ -0,0 +1,9 @@
+﻿using MediatR;
+
+namespace MealMonkey.Application.Features.CategoryFeatures.Commands.DeleteCategory
+{
+    public class DeleteCategoryCommand : IRequest<bool>
+    {
+        public Guid Id { get; set; }
+    }
+}
