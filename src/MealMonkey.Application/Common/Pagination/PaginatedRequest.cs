f2778b6206c902572f90ac01a9cf2ce3f6198a34
mohammed0xff <mohammed0xff@gmail.com>
2023-05-06 00:10:54 +0300

    feat : add response pagination.

---
+++ src/MealMonkey.Application/Common/Pagination/PaginatedRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace MealMonkey.Application.Common.Pagination
+{
+    public class PaginatedRequest
+    {
+        public int CurrentPage { get; init; } = 1;
+        public int PageSize { get; init; } = 10;
+    }
+}
